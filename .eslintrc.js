905a3ac8966daa4a8d41481b829a5848da083f02
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-07-01 02:00:21 +0200

    empty ract project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb',
+        'airbnb/hooks',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        'jsx-a11y',
+        'import',
+    ],
+    rules: {
+        // Your custom rules here
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
+
---
+++ src/redux/actions/welcomeAction.ts
@@ -0,0 +1,8 @@
+// src/actions.ts
+
+const SAY_HELLO = "SAY_HELLO";
+
+
+export const sayHello = () => ({
+  type: SAY_HELLO,
+});
