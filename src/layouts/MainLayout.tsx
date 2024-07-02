103dbd7cb3318a0ebb9c8bc78033f1b989fe78cb
kirengamartial <kirengamartial@gmail.com>
2024-07-02 08:52:15 +0200

    remove packag-lock.json and yarn lock

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
-const MainLayout = () => {
+const MainLayout: React.FC = () => {
   return (
     <>
       <Toaster/>
