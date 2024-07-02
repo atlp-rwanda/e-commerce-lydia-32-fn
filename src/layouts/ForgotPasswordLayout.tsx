0b4b875f6e8f72ba3a9b4ba1ee512620133f8187
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-07-02 13:29:37 +0200

    forgot password and reset password pages/layouts

---
+++ src/layouts/ForgotPasswordLayout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ForgotPassword from '../Components/ForgotPassword'
+
+const ForgotPasswordLayout: React.FC = () => {
+    return (
+        <ForgotPassword />
+    )
+}
+
+export default ForgotPasswordLayout
---
+++ src/layouts/ResetPasswordLayout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ResetPassword from '../Components/ResetPassword'
+
+const ResetPasswordLayout: React.FC = () => {
+    return (
+        <ResetPassword />
+    )
+}
+
+export default ResetPasswordLayout
