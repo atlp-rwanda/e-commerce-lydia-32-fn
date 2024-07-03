5994a2f3d1521efe7f0d634a27db81376b55d14f
kirengamartial <kirengamartial@gmail.com>
2024-07-03 16:14:50 +0200

    remove git from being tracked

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,22 +1,22 @@
 // src/components/Layout.tsx
-import React from 'react';
-import Navbar from '../Components/navbar';
-import Footer from '../Components/footer';
-import { Toaster } from 'react-hot-toast';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Outlet, useLocation } from 'react-router-dom';
+import React from "react";
+import Navbar from "../Components/navbar";
+import Footer from "../Components/footer";
+import { Toaster } from "react-hot-toast";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Outlet, useLocation } from "react-router-dom";
 
-const MainLayout = () => {
+const MainLayout: React.FC = () => {
   const location = useLocation();
-  const isLoginPage = location.pathname === '/login';
+  const isLoginPage = location.pathname === "/login";
 
   return (
     <div className="flex flex-col min-h-screen">
       {!isLoginPage && <Navbar />}
       <main className="flex-grow">
-        <Toaster/>
-        <ToastContainer/>
+        <Toaster />
+        <ToastContainer />
         <Outlet />
       </main>
       {!isLoginPage && <Footer />}
